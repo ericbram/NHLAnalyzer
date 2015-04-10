90d4c982af4575f1803a5af55947df8d6f4c3e9c
Eric Bram <bram.eric@gmail.com>
2015-04-09 21:49:48 -0400

    Initial Commit
    This is "quick and dirty" code, done in a couple of hours.  I don't
    think there will be any more commits, unless I want to work on making
    some type of analysis output for CSVs for graphing.

---
+++ NHLAnalyzer/NHLanalyzer.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace NHLAnalyzer
+{
+    public class NHLanalyzer
+    {
+
+        private static void Main(string[] args)
+        {
+            // ***************  these are the customizable variables for this simple scrape file *************** 
+
+            string inputfile = @"C:\Users\ebram\Desktop\stats.xml"; // this is the file from NHLScrape
+            string OURTEAM = "PIT";                                 // this is the team you are analyzing
+
+            // *************************************************************************************************
+
+            // handle the results of both a 3 goal lead, and a 3 goal deficit with Lists
+            List<AnalyzeInfo> resultsLead = new List<AnalyzeInfo>();
+            List<AnalyzeInfo> resultsBehind = new List<AnalyzeInfo>();
+
+            // deserialize the input file
+            XmlSerializer ser = new XmlSerializer(typeof (FullTeamInfo));
+            using (FileStream fs = new FileStream(inputfile, FileMode.Open))
+            {
+                FullTeamInfo fti = (FullTeamInfo) ser.Deserialize(fs);
+
+                // now that we have the object, iterate over the games
+                foreach (GameInfo gi in fti.Info)
+                {
+                    // goal differential (at every point in the game)
+                    int differential = 0;
+                    bool threeGoalLead = false;
+                    bool threeGoalDef = false;
+                    foreach (string s in gi.GoalList)
+                    {
+                        // if our team scores, we have a +1 differential, -1 for the other teams goal
+                        // with this, we can tell the goal difference after each goal
+                        if (s.Equals(OURTEAM))
+                            differential++;
+                        else
+                            differential--;
+
+                        // record if at any point in the game, we hit a +/- 3 goal differential
+                        if (differential == 3)
+                        {
+                            // at this point we have a 3 goal lead
+                            threeGoalLead = true;
+                        }
+                        else if (differential == -3)
+                        {
+                            // at this point we have a 3 goal deficit
+                            threeGoalDef = true;
+                        }
+                    }
+
+                    if (threeGoalLead)
+                    {
+                        // this was a +3 goal differential game
+                        resultsLead.Add(new AnalyzeInfo
+                                            {
+                                                GInfo = gi,
+                                                Differential = differential,
+                                                Win = differential > 0
+                                            });
+                    }
+                    else if (threeGoalDef)
+                    {
+                        // this was a -3 goal differential game
+                        resultsBehind.Add(new AnalyzeInfo
+                                              {
+                                                  GInfo = gi,
+                                                  Differential = differential,
+                                                  Win = differential > 0
+                                              });
+                    }
+                }
+            }
+
+            // this block is used to make a dictionary based on the season
+
+            // we have a key that is the season, and the value is a list of games from that season
+            Dictionary<string, List<AnalyzeInfo>> leadanalysis = new Dictionary<string, List<AnalyzeInfo>>();
+            foreach (AnalyzeInfo ai in resultsLead)
+            {
+                if (!leadanalysis.ContainsKey(ai.GInfo.DataString))
+                {
+                    leadanalysis.Add(ai.GInfo.DataString, new List<AnalyzeInfo>());
+                }
+                leadanalysis[ai.GInfo.DataString].Add(ai);
+            }
+
+            // Same as above, this time for the three goal deficit
+            Dictionary<string, List<AnalyzeInfo>> defanalysis = new Dictionary<string, List<AnalyzeInfo>>();
+            foreach (AnalyzeInfo ai in resultsBehind)
+            {
+                if (!defanalysis.ContainsKey(ai.GInfo.DataString))
+                {
+                    defanalysis.Add(ai.GInfo.DataString, new List<AnalyzeInfo>());
+                }
+                defanalysis[ai.GInfo.DataString].Add(ai);
+            }
+
+            // finally, time to do the analysis (+3 goal section)
+            // this dictionary is again indexed by season, with the values being another dictionary.  
+            // the inner dictionary is indexed by goal differential, and the value is the count of that goal differential
+            Dictionary<string, Dictionary<int, int>> leadResults = new Dictionary<string, Dictionary<int, int>>();
+
+            // loop through each season
+            foreach (string s in leadanalysis.Keys)
+            {
+                if (!leadResults.ContainsKey(s))
+                {
+                    // create a new dictionary of this season if it's not added yet
+                    leadResults.Add(s, new Dictionary<int, int>());
+                }
+                // for each game that was a +3 goal lead THIS season
+                foreach (AnalyzeInfo ai in leadanalysis[s])
+                {
+                    // if this goal differential has not been added yet, add it with a zero count
+                    if (!leadResults[s].ContainsKey(ai.Differential))
+                    {
+                        leadResults[s].Add(ai.Differential, 0);
+                    }
+
+                    // increase the amount of games with this final goal differential
+                    (leadResults[s])[ai.Differential]++;
+                }
+
+                // turn the list of final goal differentials into a list so we can sort it
+                var list = leadResults[s].Keys.ToList();
+                list.Sort();
+
+                // print the results
+                Console.WriteLine("Season:  " + s);
+                foreach (var goalDiff in list)
+                {
+                    Console.WriteLine("Final Differential of " + goalDiff + ":  " + (leadResults[s])[goalDiff]);
+                }
+            }
+
+            // space it out
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            // do the analysis (-3 goal section)
+            // this dictionary is again indexed by season, with the values being another dictionary.  
+            // the inner dictionary is indexed by goal differential, and the value is the count of that goal differential
+            Dictionary<string, Dictionary<int, int>> defResults = new Dictionary<string, Dictionary<int, int>>();
+
+            // loop through each season
+            foreach (string s in defanalysis.Keys)
+            {
+                if (!defResults.ContainsKey(s))
+                {
+                    // create a new dictionary of this season if it's not added yet
+                    defResults.Add(s, new Dictionary<int, int>());
+                }
+
+                // for each game that was a -3 goal lead THIS season
+                foreach (AnalyzeInfo ai in defanalysis[s])
+                {
+                    // if this goal differential has not been added yet, add it with a zero count
+                    if (!defResults[s].ContainsKey(ai.Differential))
+                    {
+                        defResults[s].Add(ai.Differential, 0);
+                    }
+
+                    // increase the amount of games with this final goal differential
+                    (defResults[s])[ai.Differential]++;
+                }
+
+                // turn the list of final goal differentials into a list so we can sort it
+                var list = defResults[s].Keys.ToList();
+                list.Sort();
+
+                // print the results
+                Console.WriteLine("Season:  " + s);
+                foreach (var goalDiff in list)
+                {
+                    Console.WriteLine("Final Differential of " + goalDiff + ":  " + (defResults[s])[goalDiff]);
+                }
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+
+    public struct AnalyzeInfo
+    {
+        private GameInfo gInfo;
+        private bool win;
+        private int differential;
+
+        public GameInfo GInfo
+        {
+            get { return gInfo; }
+            set { gInfo = value; }
+        }
+
+        public bool Win
+        {
+            get { return win; }
+            set { win = value; }
+        }
+
+        public int Differential
+        {
+            get { return differential; }
+            set { differential = value; }
+        }
+    }
+
+    public struct FullTeamInfo
+    {
+        private List<GameInfo> info;
+
+        [XmlArray("Info"), XmlArrayItem(typeof(GameInfo), ElementName = "GameInfo")]
+        public List<GameInfo> Info
+        {
+            get { return info; }
+            set { info = value; }
+        }
+    }
+
+    [XmlRoot("GameInfo")]
+    public struct GameInfo
+    {
+        private DateTime date;
+        private string dataString;
+        private String opponent;
+        private List<string> goalList;
+
+        public List<string> GoalList
+        {
+            get { return goalList; }
+            set { goalList = value; }
+        }
+
+        public DateTime Date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+
+        public string DataString
+        {
+            get { return dataString; }
+            set { dataString = value; }
+        }
+
+        public string Opponent
+        {
+            get { return opponent; }
+            set { opponent = value; }
+        }
+    }
+}
---
+++ NHLScrape/NHLScrape.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+using HtmlAgilityPack;
+
+namespace NHLScrape
+{
+    public class NHLScrape
+    {
+        private static void Main(string[] args)
+        {
+            // *****  these are the customizable variables for this simple scrape file ***** 
+
+            int yearstart = 2007;                                   // year to start recording.  For instance, "2007" starts with the 2007-2008 season
+            int yearend = 2015;                                     // year to end recording.  For instance, "2015" ends with 2014-2015 season
+            string LOCALTEAM = "Pittsburgh";                        // team name
+            string LOCALTEAMABV = "PIT";                            // team abbreviation
+            string statsFile = @"C:\Users\ebram\Desktop\stats.xml"; // where to save the xml file
+
+            // ******************************************************************************
+
+
+
+
+            FullTeamInfo games = new FullTeamInfo{Info = new List<GameInfo>()};
+
+            for (int years = yearstart; years < yearend; years++)
+            {
+                HtmlWeb web = new HtmlWeb();
+                HtmlDocument doc =
+                    web.Load("http://www.nhl.com/ice/schedulebyseason.htm?season=" +
+                             years.ToString() +
+                             (years + 1).ToString() +
+                             "&team=" + LOCALTEAMABV);
+
+                // foreach season defined
+                foreach (HtmlNode l in doc.DocumentNode.SelectNodes("//*[contains(concat(' ', @class, ' '), ' data ')]")
+                    )
+                {
+                    // foreach game
+                    foreach (HtmlNode link in l.SelectNodes(".//tbody/tr"))
+                    {
+                        // get the date using xpath
+                        string date = "";
+                        try
+                        {
+                            HtmlNode tmpnode =
+                                link.SelectSingleNode(".//*[contains(concat(' ', @class, ' '), ' skedStartDateSite ')]");
+                            if (tmpnode != null)
+                            {
+                                date = tmpnode.InnerHtml;
+                            }
+                            else
+                            {
+                                // might be a row with nothing
+                                continue;
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            // might be a row with nothing or malformed
+                            continue;
+                        }
+
+                        // had a real date so it must be a real game
+
+                        // get the 2 teams using xpath
+                        HtmlNodeCollection teams =
+                            link.SelectNodes(".//*[contains(concat(' ', @class, ' '), ' teamName ')]");
+                        string team = "";
+                        string team1 = teams[0].InnerText;
+                        string team2 = teams[1].InnerText;
+
+                        // since we are defining who "our" team is, only store the one that is the opponent
+                        team = team1.Equals(LOCALTEAM) ? team2 : team1;
+
+                        // most important part, get the URL for the actual game recap
+                        HtmlNode html =
+                            link.SelectSingleNode(".//*[contains(concat(' ', @class, ' '), ' skedLinks ')]/a");
+                        string hreflink = html.GetAttributeValue("href", "");
+
+                        // create most of the game info
+                        GameInfo gi = new GameInfo
+                                          {
+                                              Date = DateTime.Parse(date),
+                                              Opponent = team,
+                                              DataString = years.ToString() + " - " + (years + 1).ToString(),
+                                              GoalList = new List<string>()
+                                          };
+
+                        if (DateTime.Now < DateTime.Parse(date))
+                        {
+                            // don't count games that haven't happened yet
+                            continue;
+                        }
+
+                        // now we need the actual game score
+                        HtmlDocument gamedoc = web.Load(hreflink);
+
+                        // sections lists all of the goals that occurred
+                        HtmlNodeCollection sections =
+                            gamedoc.DocumentNode.SelectNodes("//*[contains(concat(' ', @class, ' '), ' section ')]");
+
+                        HtmlNode gamenode = null;
+
+
+                        // sections may be null if no goals were scored
+                        if (sections != null)
+                        {
+                            foreach (HtmlNode hn in sections)
+                            {
+                                // find the section of the page that is the scoring summary
+                                if (hn.InnerHtml.Contains("scoring summary"))
+                                {
+                                    gamenode = hn;
+                                }
+                            }
+                        }
+
+                        // gamenode could be null if this is malformed or no goals were scored
+                        if (gamenode != null)
+                        {
+                            try
+                            {
+                                // get a list of goals based on xpath
+                                HtmlNodeCollection col =
+                                    gamenode.SelectNodes(".//*[contains(concat(' ', @class, ' '), ' timeAndTeam ')]");
+                                if (col != null && col.Count != 0)
+                                {
+                                    // if the list of goals is found and greater than zero, we have some goals to record!
+                                    foreach (
+                                        HtmlNode hn in
+                                            gamenode.SelectNodes(
+                                                ".//*[contains(concat(' ', @class, ' '), ' timeAndTeam ')]"))
+                                    {
+                                        // this is a goal, add it to the list
+                                        // note that by default this will be stored in chronological order
+                                        gi.GoalList.Add(hn.ChildNodes[2].InnerText);
+                                    }
+                                }
+                            }
+                            catch (Exception E)
+                            {
+                                
+                            }
+
+                        }
+
+                        // we have added all of the goals to this info so we can conclude this game
+                        games.Info.Add(gi);
+                    }
+                }
+            }
+
+            
+            // now we need to save this file
+            XmlSerializer ser = new XmlSerializer(typeof(FullTeamInfo));
+            using (FileStream fs = new FileStream(statsFile, FileMode.Create))
+            {
+                ser.Serialize(fs, games);
+            }
+        }
+
+
+        public struct FullTeamInfo
+        {
+            private List<GameInfo> info;
+            [XmlArray("Info"), XmlArrayItem(typeof(GameInfo), ElementName = "GameInfo")]
+            public List<GameInfo> Info
+            {
+                get { return info; }
+                set { info = value; }
+            }
+        }
+
+        [XmlRoot("GameInfo")]
+        public struct GameInfo
+        {
+            private DateTime date;
+            private string dataString;
+            private String opponent;
+            private List<string> goalList;
+
+            public List<string> GoalList
+            {
+                get { return goalList; }
+                set { goalList = value; }
+            }
+
+            public DateTime Date
+            {
+                get { return date; }
+                set { date = value; }
+            }
+
+            public string DataString
+            {
+                get { return dataString; }
+                set { dataString = value; }
+            }
+
+            public string Opponent
+            {
+                get { return opponent; }
+                set { opponent = value; }
+            }
+        }
+    }
+}
